4fd06a6ea364ce4b8ff9f26048eb44e05e5c71ff
jonasSalviano <jonasalmeida49@gmail.com>
2026-01-06 19:52:34 -0300

    Resolvendo conflito

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 function Home() {
   return (
     <div>
-<<<<<<< HEAD
       <h1>Bem vindo</h1>
-=======
-      <h1>Bem vindo a pagina</h1>
->>>>>>> 8e682b3ffc1ac8f10df3edf97e1c7f90cbe3711f
     </div>
   );
 }
