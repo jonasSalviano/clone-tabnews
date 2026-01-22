db98615b134e7be709f18fe1749c6681cf61ef2f
jonasSalviano <jonasalmeida49@gmail.com>
2026-01-22 19:36:17 -0300

    add `database` ande `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+async function status(request, response) {
+  const consult = await database.query("Select 1 + 1 as sum;");
+  console.log(consult.rows);
   response.status(200).json({ status: "ok" });
 }
 
