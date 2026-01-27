2d4834f9968283bcb04f860323c6f5e34e9e4367
jonasSalviano <jonasalmeida49@gmail.com>
2026-01-26 21:50:05 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,26 @@
 import database from "infra/database.js";
+
 async function status(request, response) {
-  const consult = await database.query("Select 1 + 1 as sum;");
-  console.log(consult.rows);
-  response.status(200).json({ status: "ok" });
+  const updateAt = new Date().toISOString();
+  const version = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+
+  const dataBaseName = process.env.POSTGRES_DB;
+  const connectionsActive = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dataBaseName],
+  });
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version_db: version.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        connections_active: connectionsActive.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,19 @@
 test("GET /api/v1/status should returns 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
-  expect(res.status).toBe(200);
   const data = await res.json();
-  expect(data).toEqual({ status: "ok" });
+  const parsedUpdateAt = new Date(data.update_at).toISOString();
+  expect(data.update_at).toEqual(parsedUpdateAt);
+
+  expect(data.dependencies.database).toHaveProperty("version_db");
+  expect(data.dependencies.database).toHaveProperty("max_connections");
+  expect(data.dependencies.database).toHaveProperty("connections_active");
+
+  expect(typeof data.dependencies.database.version_db).toBe("string");
+  expect(typeof data.dependencies.database.max_connections).toBe("number");
+  expect(typeof data.dependencies.database.connections_active).toBe("number");
+
+  expect(data.dependencies.database.max_connections).toEqual(100);
+  expect(data.dependencies.database.connections_active).toEqual(1);
+
+  expect(data.dependencies.database.version_db).toMatch(/^\d+(\.\d+)?$/);
 });
