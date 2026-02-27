b898af2da28ac3e0ed1cdecf29980781f0e8f1b1
jonasSalviano <jonasalmeida49@gmail.com>
2026-02-26 22:44:39 -0300

    refactor: use `next-connect` in `/migrations` and `/status`

--- infra/errors.js
+++ infra/errors.js
@@ -1,9 +1,9 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Um erro interno nao esperado aconteceu.", { cause });
     this.name = "InternalServerError";
     this.action = "Entre em contato com o suporte.";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
   }
 
   toJSON() {
@@ -11,7 +11,44 @@ export class InternalServerError extends Error {
       name: this.name,
       message: this.message,
       action: this.action,
-      statusCode: this.statusCode,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Servico indisponivel no momento", { cause });
+    this.name = "ServiceError";
+    this.action = "Verifique se o servico esta disponivel.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Metodo nao permitido para este endpoint.");
+    this.name = "MethodNotAllowed";
+    this.action =
+      "Verifique se o metodo HTTP enviado e valido para este endpoint.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
     };
   }
 }
