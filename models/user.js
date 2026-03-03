0e297c4fe6094e33b473c21376e095af75766612
jonasSalviano <jonasalmeida49@gmail.com>
2026-03-02 23:24:15 -0300

    feat: create `user` model and `/api/v1/users` endpoint

---
+++ models/user.js
@@ -0,0 +1,71 @@
+import database from "infra/database";
+import { ValidationError } from "infra/errors";
+
+async function create(userInputValue) {
+  await validatUniqueEmail(userInputValue.email);
+  await validatUniqueUsername(userInputValue.username);
+
+  const newUser = await runInsertQuery(userInputValue);
+  return newUser;
+
+  async function validatUniqueEmail(email) {
+    const result = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        LOWER(email) = LOWER($1)`,
+      values: [email],
+    });
+    if (result.rows.length > 0) {
+      throw new ValidationError({
+        message: "O email informado ja existe.",
+        action: "Utilize um email diferente.",
+      });
+    }
+  }
+
+  async function validatUniqueUsername(username) {
+    const usernameResult = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        LOWER(username) = LOWER($1)`,
+      values: [username],
+    });
+    if (usernameResult.rows.length > 0) {
+      throw new ValidationError({
+        message: "O nome de usuario informado ja existe.",
+        action: "Utilize um nome de usuario diferente.",
+      });
+    }
+  }
+
+  async function runInsertQuery(userInputValue) {
+    const result = await database.query({
+      text: `
+        INSERT INTO
+          users (username, password, email)
+        VALUES
+          ($1, $2, $3)
+        RETURNING *`,
+      values: [
+        userInputValue.username,
+        userInputValue.password,
+        userInputValue.email,
+      ],
+    });
+    return result.rows[0];
+  }
+}
+
+const user = {
+  create,
+};
+
+export default user;
---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,86 @@
+import { version as uuidVersion } from "uuid";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: "user",
+          password: "password123",
+          email: "user@example.com",
+        }),
+      });
+      expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "user",
+        password: "password123",
+        email: "user@example.com",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+
+    test("With duplicate 'email'", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: "user2",
+          password: "password123",
+          email: "User@example.com",
+        }),
+      });
+
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "ValidationError",
+        message: "O email informado ja existe.",
+        action: "Utilize um email diferente.",
+        status_code: 400,
+      });
+    });
+
+    test("With duplicate 'username'", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: "user",
+          password: "password123",
+          email: "user2@example.com",
+        }),
+      });
+
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "ValidationError",
+        message: "O nome de usuario informado ja existe.",
+        action: "Utilize um nome de usuario diferente.",
+        status_code: 400,
+      });
+    });
+  });
+});
