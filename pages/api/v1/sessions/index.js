6e6d075f56b5b764388ed701feb89ac429d0c0f6
jonasSalviano <jonasalmeida49@gmail.com>
2026-03-13 21:48:48 -0300

    feat: implement session creation

---
+++ pages/api/v1/sessions/index.js
@@ -0,0 +1,31 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller";
+import authentication from "models/authentication";
+import session from "models/session";
+import * as cookie from "cookie";
+
+const router = createRouter();
+
+router.post(postHandle);
+
+export default router.handler(controller.errorHandlers);
+
+async function postHandle(request, response) {
+  const userInputValue = request.body;
+  const authenticatedUser = await authentication.getAuthenticatedUser(
+    userInputValue.email,
+    userInputValue.password,
+  );
+  const newSession = await session.create(authenticatedUser.id);
+
+  const setCookie = cookie.serialize("session_id", newSession.token, {
+    path: "/",
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  response.setHeader("Set-Cookie", setCookie);
+
+  return response.status(201).json(newSession);
+}
